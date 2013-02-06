55f4859f2e12aafbce236feeebb887bacbee3a1a
Sho Hashimoto <hashimoto@shokai.org>
2013-02-07 06:47:20 +0900

    fix command injection

--- controllers/main.rb
+++ controllers/main.rb
@@ -9,7 +9,7 @@ end
 
 get '/links.json' do
   @url = params[:url]
-  link = `phantomjs bin/get_links.js #{@url}`.
+  link = `phantomjs bin/get_links.js '#{@url}'`.
     split(/[\r\n]/).
     select{|i| i =~ /^links=/ }.first
   halt 500, 'error' unless link
